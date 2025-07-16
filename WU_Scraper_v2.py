2681bac4193b05578ba5a52fb1230662c6f8038f
Alejandro Verduzco <84338955+JAVerduzco@users.noreply.github.com>
2025-07-16 13:28:12 -0700

    Update WU_Scraper_v2.py
    st cache data added

--- WU_Scraper_v2.py
+++ WU_Scraper_v2.py
@@ -3,21 +3,19 @@ import pandas as pd
 import requests
 from datetime import datetime
 
-
 st.set_page_config(layout='wide')
+st.header("Pronósticos a 10 días")
 
-st.header("Pronósticos  a 10 días V2")
-
-
-
-urls_ciudades = ['https://api.weather.com/v3/wx/forecast/daily/10day?apiKey=e1f10a1e78da46f5b10a1e78da96f525&geocode=30.496%2C-112.323&units=m&language=en-US&format=json', #Caborca
-                 'https://api.weather.com/v3/wx/forecast/daily/10day?apiKey=e1f10a1e78da46f5b10a1e78da96f525&geocode=29.1%2C-110.951&units=m&language=en-US&format=json',   #Hermosillo
-                 'https://api.weather.com/v3/wx/forecast/daily/10day?apiKey=e1f10a1e78da46f5b10a1e78da96f525&geocode=27.471%2C-109.929&units=m&language=en-US&format=json', #Obregon
-                 'https://api.weather.com/v3/wx/forecast/daily/10day?apiKey=e1f10a1e78da46f5b10a1e78da96f525&geocode=25.792%2C-108.992&units=m&language=en-US&format=json', # Los Mochis
-                 'https://api.weather.com/v3/wx/forecast/daily/10day?apiKey=e1f10a1e78da46f5b10a1e78da96f525&geocode=24.782%2C-107.395&units=m&language=en-US&format=json'  # Culiacan
- ]
+urls_ciudades = [
+    'https://api.weather.com/v3/wx/forecast/daily/10day?apiKey=e1f10a1e78da46f5b10a1e78da96f525&geocode=30.496%2C-112.323&units=m&language=en-US&format=json',  # Caborca
+    'https://api.weather.com/v3/wx/forecast/daily/10day?apiKey=e1f10a1e78da46f5b10a1e78da96f525&geocode=29.1%2C-110.951&units=m&language=en-US&format=json',    # Hermosillo
+    'https://api.weather.com/v3/wx/forecast/daily/10day?apiKey=e1f10a1e78da46f5b10a1e78da96f525&geocode=27.471%2C-109.929&units=m&language=en-US&format=json',  # Obregon
+    'https://api.weather.com/v3/wx/forecast/daily/10day?apiKey=e1f10a1e78da46f5b10a1e78da96f525&geocode=25.792%2C-108.992&units=m&language=en-US&format=json',  # Los Mochis
+    'https://api.weather.com/v3/wx/forecast/daily/10day?apiKey=e1f10a1e78da46f5b10a1e78da96f525&geocode=24.782%2C-107.395&units=m&language=en-US&format=json'   # Culiacan
+]
 ciudades = ['CAB', 'HMO', 'OBR', 'LMO', 'CUL']
 
+@st.cache_data
 def obtener_datos(url):
     response = requests.get(url)
     data = response.json()
@@ -27,26 +25,21 @@ df = pd.DataFrame()
 
 for i, url in enumerate(urls_ciudades):
     datos = obtener_datos(url)
-
     if datos:  # Solo procesar si se obtuvieron datos
-          df[f'{ciudades[i]}_TempMax'] = datos['calendarDayTemperatureMax'] # Valores correspondientes a la key'calendarDayTemperatureMax'
+        df[f'{ciudades[i]}_TempMax'] = datos['calendarDayTemperatureMax']  # Valores correspondientes a la key 'calendarDayTemperatureMax'
 
 for i, url in enumerate(urls_ciudades):
     datos = obtener_datos(url)
-
     if datos:  # Solo procesar si se obtuvieron datos
-          df[f'{ciudades[i]}_TempMin'] = datos['calendarDayTemperatureMin'] # Valores correspondientes a la key 'calendarDayTemperatureMin'
-
+        df[f'{ciudades[i]}_TempMin'] = datos['calendarDayTemperatureMin']  # Valores correspondientes a la key 'calendarDayTemperatureMin'
 
 for i, url in enumerate(urls_ciudades):
     datos = obtener_datos(url)
-
     if datos:  # Solo procesar si se obtuvieron datos
-          if i == 0:
+        if i == 0:
             continue
-          else:
-            df[f'{ciudades[i]}_QPF'] = datos['qpf'] # Valores correspondientes a la key 'qpf'
-
+        else:
+            df[f'{ciudades[i]}_QPF'] = datos['qpf']  # Valores correspondientes a la key 'qpf'
 
 fechas = obtener_datos(urls_ciudades[0])['sunriseTimeLocal']
 
@@ -61,9 +54,8 @@ formatted_dates = [formato_fecha(date) for date in fechas]
 # Asignar las fechas como índices
 df.index = formatted_dates
 
-
 df = df[:-1]
 
-# Impresioon de tabla en pantalla
-st.dataframe(df,use_container_width=True)
-# st.table(df)
+# Impresión de tabla en pantalla
+st.dataframe(df, use_container_width=True)
+
